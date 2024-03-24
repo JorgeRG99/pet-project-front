6a9ebd899f5878165f932bdae56bceaf182822cf
Jorgito Ramírez Galvan <jorgeramirezgalvan99@gmail.com>
2024-03-24 10:37:00 +0100

    add description to navbar links

--- src/pages-components/Navbar.jsx
+++ src/pages-components/Navbar.jsx
@@ -50,55 +50,53 @@ const services = [
 
 export function Navbar() {
   return (
-    <header className="flex justify-between items-center border-b-2 border-primary-extra-light">
-      <div className="pl-2">
+    <header className="flex justify-between items-center w-full px-4 border-b-2 border-primary-extra-light">
+      <div className="">
         <PetLogo />
       </div>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="text-md">
-                Mascotas
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {pets.map((component) => (
-                    <ListItem
-                      key={crypto.randomUUID()}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="text-md">
-                Servicios
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {services.map((component) => (
-                    <ListItem
-                      key={crypto.randomUUID()}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      <div className="w-[250px] flex justify-evenly">
+      <NavigationMenu className="absolute left-1/2 transform -translate-x-1/2">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-md">
+              Mascotas
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {pets.map((component) => (
+                  <ListItem
+                    key={crypto.randomUUID()}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-md">
+              Servicios
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {services.map((component) => (
+                  <ListItem
+                    key={crypto.randomUUID()}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="space-x-4">
         <Button variant="ghost">Acceder</Button>
-        <Button className="text-white">
-          Registro
-        </Button>
+        <Button className="text-white">Registro</Button>
       </div>
     </header>
   );
@@ -111,6 +109,7 @@ const ListItem = React.forwardRef(
         <NavigationMenuLink asChild>
           <Link
             to={ref}
+            title={children}
             className={cn(
               "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
               className
